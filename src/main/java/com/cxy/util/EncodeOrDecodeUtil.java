bfe9dd0ead743ee530ad051ca1a9348ee8c2d095
chenxianyong <1051217793@qq.com>
2019-07-12 17:34:56 +0800

    EncodeOrDecodeUtil

--- src/main/java/com/cxy/util/EncodeOrDecodeUtil.java
+++ src/main/java/com/cxy/util/EncodeOrDecodeUtil.java
@@ -19,7 +19,7 @@ public class EncodeOrDecodeUtil {
 	 * @author XianyongChen
 	 * @date 2019年7月12日
 	 * @param encodeManner 加密方式
-	 * @param str          需要加密的字符串
+	 * @param str 需要加密的字符串
 	 * @return 加密后的字符串
 	 */
 	public static String encode(int encodeManner, String str) {
@@ -34,6 +34,9 @@ public class EncodeOrDecodeUtil {
 		case 3:// BASE64
 			result = base64Encode(str);
 			break;
+		case 4://HMAC
+			result = hmacEncode(str);
+			break;
 		default:
 			break;
 		}
@@ -45,7 +48,7 @@ public class EncodeOrDecodeUtil {
 	 * @author XianyongChen
 	 * @date 2019年7月12日
 	 * @param encodeManner 加密方式，参考枚举EncodeManner
-	 * @param inStr        需要加密的字符串
+	 * @param inStr 需要加密的字符串
 	 * @return 加密后的字符串(MD5:32位;sha1:40位。
 	 */
 	static String md5Encode(String inStr) {
@@ -106,29 +109,6 @@ public class EncodeOrDecodeUtil {
 	}
 
 	/**
-	 * @author : XianyongChen
-	 * @data : 2019年7月12日 - 下午4:38:31
-	 * @description :加密方式
-	 */
-	public static enum EncodeManner {
-		MD5(1), SHA1(2), BASE64(3);
-		int index;
-
-		EncodeManner(int index) {
-			this.index = index;
-		}
-
-		public int getIndex() {
-			return index;
-		}
-
-		public void setIndex(int index) {
-			this.index = index;
-		}
-
-	}
-
-	/**
 	 * @Description SHA加密
 	 * @author XianyongChen
 	 * @date 2019年7月12日
@@ -161,6 +141,29 @@ public class EncodeOrDecodeUtil {
 	}
 
 	/**
+	 * @author : XianyongChen
+	 * @data : 2019年7月12日 - 下午4:38:31
+	 * @description :加密方式
+	 */
+	public static enum EncodeManner {
+		MD5(1), SHA1(2), BASE64(3), HMAC(4);
+		int index;
+
+		EncodeManner(int index) {
+			this.index = index;
+		}
+
+		public int getIndex() {
+			return index;
+		}
+
+		public void setIndex(int index) {
+			this.index = index;
+		}
+
+	}
+
+	/**
 	 * @Description HMAC加密
 	 * @author XianyongChen
 	 * @date 2019年7月12日
@@ -183,6 +186,5 @@ public class EncodeOrDecodeUtil {
 		}
 		return result;
 	}
-
 
 }
